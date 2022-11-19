cc332feac3d074d7f2e66bd0c33d64948c0e9cf2
dldbstls <2younsin@gmail.com>
2022-11-19 21:12:05 +0900

    draw board

--- main.c
+++ main.c
@@ -1,30 +1,130 @@
-#include <ncurses.h>
+/* senet.c */
 
-int main(int argc, char const *argv[]) {
-    WINDOW *window;
+#include <ncurses.h>
+#include <stdlib.h>
+
+#define SQ_HEIGHT 5
+#define SQ_WIDTH 12
+#define NUM_ROW 4
+#define NUM_COL 4
+
+void create_board(void);
+void destroy_board(void);
+
+WINDOW *BOARD[16];
+
+int main(int argc, char **argv) {
+    int key;
+
+    /* initialize curses */
 
     initscr();
+    noecho();
+    curs_set(FALSE);
 
-    if (has_colors() == FALSE) {
-        puts("Terminal does not support colors!");
+    if ((LINES < 24) || (COLS < 80)) {
         endwin();
-        return 1;
-    } else {
-        start_color();
-        init_pair(1, COLOR_BLUE, COLOR_WHITE);
+        puts("Your terminal needs to be at least 80x24");
+        exit(2);
     }
 
+    /* print welcome text */
+    clear();
+
+    mvprintw(0, 0, "2048");
     refresh();
 
-    window = newwin(18, 80, 0, 0);
+    /* draw board */
+    create_board();
 
-    wbkgd(window, COLOR_PAIR(1));
+    do {
+        key = getch();
+    } while ((key != 'q') && (key != 'Q'));
 
-    wprintw(window, "Hello!");
+    /* when done, free up the board, and exit */
+    destroy_board();
 
-    wrefresh(window);
-
-    getch();
     endwin();
-    return 0;
+    exit(0);
 }
+
+void create_board(void) {
+    int i;
+    int starty, startx;
+
+    starty = 5;
+    for (i = 0; i < NUM_COL; i++) {
+        startx = 10 + i * SQ_WIDTH;
+        BOARD[i] = newwin(SQ_HEIGHT, SQ_WIDTH, starty, startx);
+    }
+
+    starty += SQ_HEIGHT;
+    for (i = NUM_COL; i < NUM_COL * 2; i++) {
+        startx = 10 + (i % 4) * SQ_WIDTH;
+        BOARD[i] = newwin(SQ_HEIGHT, SQ_WIDTH, starty, startx);
+    }
+
+    starty += SQ_HEIGHT;
+    for (i = NUM_COL * 2; i < NUM_COL * 3; i++) {
+        startx = 10 + (i % 4) * SQ_WIDTH;
+        BOARD[i] = newwin(SQ_HEIGHT, SQ_WIDTH, starty, startx);
+    }
+
+    starty += SQ_HEIGHT;
+    for (i = NUM_COL * 3; i < NUM_COL * 4; i++) {
+        startx = 10 + (i % 4) * SQ_WIDTH;
+        BOARD[i] = newwin(SQ_HEIGHT, SQ_WIDTH, starty, startx);
+    }
+
+    /* put border on each window and refresh */
+
+    for (i = 0; i < 30; i++) {
+        box(BOARD[i], 0, 0);
+        wrefresh(BOARD[i]);
+    }
+}
+
+void destroy_board(void) {
+    int i;
+
+    /* erase every box and delete each window */
+
+    for (i = 0; i < 30; i++) {
+        wborder(BOARD[i], ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+        wrefresh(BOARD[i]);
+
+        delwin(BOARD[i]);
+    }
+}
+
+// int timer() {
+//     int row = 10, col = 10;
+//     initscr();
+//     noecho();
+//     curs_set(FALSE);
+
+//     keypad(stdscr, TRUE);
+//     addch(ACS_ULCORNER);
+//     while (1) {
+//         int input = getch();
+//         clear();
+//         switch (input) {
+//         case KEY_UP:
+//             mvprintw(row += 3, col, "[ A ]");
+//             continue;
+//         case KEY_DOWN:
+//             mvprintw(row -= 3, col, "[ A ]");
+//             continue;
+//         case KEY_LEFT:
+//             mvprintw(row, col -= 5, "[ A ]");
+//             continue;
+//         case KEY_RIGHT:
+//             mvprintw(row, col += 5, "[ A ]");
+//             continue;
+//         }
+//         if (input == 'q')
+//             break;
+//     }
+//     endwin();
+//     return 0;
+// }
