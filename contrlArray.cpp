e48a9e2668cd08187ee26c1f8d418648939ef284
dldbstls <2younsin@gmail.com>
2022-11-21 12:15:10 +0900

    Do not merge what has been merged

--- contrlArray.cpp
+++ contrlArray.cpp
@@ -10,15 +10,18 @@ bool is_possible_moving(int (*compare_arr)[4], int (*target_arr)[4]) {
     return false;
 }
 
-
-
 void move_block_left(blockLog *test) {
     int new_col = 0;
+    int stop_point = 0;
+
     for (int row = 0; row < 4; row++) {
+        stop_point = 0;
+
         for (int col = 0; col < 4; col++) {
             new_col = col;
             while (new_col > 0 && new_col < 4) {
-                if (((test->state).at(row)).at(new_col) == 0) {
+                if (((test->state).at(row)).at(new_col) == 0 ||
+                    new_col <= stop_point) {
                     break;
                 }
                 // if the value on the left is 0
@@ -34,6 +37,9 @@ void move_block_left(blockLog *test) {
                     ((test->state).at(row)).at(new_col - 1) *= 2;
                     ((test->state).at(row)).at(new_col) = 0;
                     new_col--;
+
+                    // Update stop_point
+                    stop_point = new_col;
                 }
                 // if the value on the left is not 0 and is different from
                 // itself
@@ -46,11 +52,16 @@ void move_block_left(blockLog *test) {
 }
 void move_block_right(blockLog *test) {
     int new_col = 0;
+    int stop_point = 4 - 1;
+
     for (int row = 0; row < 4; row++) {
+        stop_point = 4 - 1;
+
         for (int col = 4 - 1; col >= 0; col--) {
             new_col = col;
             while (new_col >= 0 && new_col < 4 - 1) {
-                if (((test->state).at(row)).at(new_col) == 0) {
+                if (((test->state).at(row)).at(new_col) == 0 ||
+                    new_col >= stop_point) {
                     break;
                 }
                 // if the value on the right is 0
@@ -58,14 +69,15 @@ void move_block_right(blockLog *test) {
                     ((test->state).at(row)).at(new_col + 1) =
                         ((test->state).at(row)).at(new_col);
                     ((test->state).at(row)).at(new_col) = 0;
-                    new_col = new_col + 1;
+                    new_col++;
                 }
                 // if the value on the right is equal to its value
                 else if (((test->state).at(row)).at(new_col + 1) ==
                          ((test->state).at(row)).at(new_col)) {
                     ((test->state).at(row)).at(new_col + 1) *= 2;
                     ((test->state).at(row)).at(new_col) = 0;
-                    new_col = new_col + 1;
+                    new_col++;
+                    stop_point = new_col;
                 }
                 // if the value on the right is not 0 and is different from
                 // itself
@@ -78,12 +90,17 @@ void move_block_right(blockLog *test) {
 }
 void move_block_up(blockLog *test) {
     int new_row = 0;
+    int stop_point = 0;
+
     for (int col = 0; col < 4; col++) {
+        stop_point = 0;
+
         for (int row = 0; row < 4; row++) {
             new_row = row;
             while (new_row > 0 && new_row < 4) {
                 // if the value of the cureent block is 0
-                if (((test->state).at(new_row)).at(col) == 0) {
+                if (((test->state).at(new_row)).at(col) == 0 ||
+                    new_row <= stop_point) {
                     break;
                 }
                 // if the value above is 0
@@ -99,6 +116,7 @@ void move_block_up(blockLog *test) {
                     ((test->state).at(new_row - 1)).at(col) *= 2;
                     ((test->state).at(new_row)).at(col) = 0;
                     new_row--;
+                    stop_point = new_row;
                 }
                 // if the value above is not 0 and is different from itself
                 else {
@@ -110,12 +128,16 @@ void move_block_up(blockLog *test) {
 }
 void move_block_down(blockLog *test) {
     int new_row = 0;
+    int stop_point = 4 - 1;
     for (int col = 0; col < 4; col++) {
+        stop_point = 4 - 1;
+
         for (int row = 4 - 1; row >= 0; row--) {
             new_row = row;
             while (new_row >= 0 && new_row < 4 - 1) {
                 // if the value of the current block is 0
-                if (((test->state).at(new_row)).at(col) == 0) {
+                if (((test->state).at(new_row)).at(col) == 0 ||
+                    new_row >= stop_point) {
                     break;
                 }
                 // if the value below is 0
@@ -131,6 +153,7 @@ void move_block_down(blockLog *test) {
                     ((test->state).at(new_row + 1)).at(col) *= 2;
                     ((test->state).at(new_row)).at(col) = 0;
                     new_row++;
+                    stop_point = new_row;
                 }
                 // if the value above is not 0 and is different from itself
                 else {
